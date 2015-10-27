84b977b4613d1258c3243d3a98b255ec65c78a68
Ron Balthazor <ron.balthazor@gmail.com>
2015-10-27 16:44:25 -0400

    profile bug fix

--- src/Sustain/UserBundle/Entity/UserRepository.php
+++ src/Sustain/UserBundle/Entity/UserRepository.php
@@ -38,7 +38,7 @@ class UserRepository extends EntityRepository
         return $this->createQueryBuilder('u')
           ->andWhere('u.public = :public')
           ->andWhere('u.role >= :role')
-          ->andWhere('u.id >= :id')
+          ->andWhere('u.id = :id')
           ->setParameter('public', '0')
           ->setParameter('role', '2')
           ->setParameter('id', $id)
